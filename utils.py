1581250947d2ea077c4a92b7b322a09cd739adc4
Antonino Ingargiola <tritemio@gmail.com>
2017-08-23 16:35:33 -0700

    Update notebooks

---
+++ utils.py
@@ -0,0 +1,162 @@
+"""Utility functions used by the notebooks
+"""
+
+import json
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+pd.options.display.max_columns = 48
+pd.options.display.max_rows = 48
+
+
+def cal_phrate_alex(d, stream, phrates=None, recompute=False):
+    """Compute peak photon rate in bursts for all streams (1-spot ALEX version)
+    """
+    if phrates is None:
+        phrates = {}
+    phrates_fname = Path('results/%s_phrate_%s.csv' % (mlabel_alex, stream))
+    phrates_fnameB = Path('results/%s_phrate_%sB.csv' % (mlabel_alex, stream))
+    if phrates_fname.is_file() and not recompute:
+        with open(phrates_fname) as f:
+            phrates[str(stream)] = json.load(f)
+        phr = pd.read_csv(phrates_fnameB, index_col=0)
+        phr.columns = [int(c) for c in phr.columns]
+        phr.columns.name = 'spot'
+        phrates[str(stream)+'B'] = phr
+    else:
+        try:
+            d.calc_max_rate(m=10, ph_sel=stream, compact=True)
+        except ValueError:
+            d.calc_max_rate(m=10, ph_sel=stream, compact=False)
+        phrates[str(stream)+'B'] = make_df_bursts_alex(d.max_rate)
+        phrates[str(stream)] = {
+                        'num_bursts': int(d.num_bursts),
+                        'num_nans': int(np.isnan(d.max_rate[0]).sum())}
+        phrates[str(stream)]['num_valid'] = int(d.num_bursts - phrates[str(stream)]['num_nans'])
+        phrates[str(stream)]['valid_fraction'] = float(100 * phrates[str(stream)]['num_valid'] / d.num_bursts)
+
+        phrates_fname.parent.mkdir(parents=True, exist_ok=True)
+        with open(phrates_fname, 'wt') as f:
+            json.dump(phrates[str(stream)], f)
+        phrates[str(stream)+'B'].to_csv(phrates_fnameB)
+
+    print('   Valid fraction (mean of all ch): %.1f %%' %
+          np.mean(phrates[str(stream)]['valid_fraction']))
+    return phrates
+
+
+def make_df_bursts_alex(list_of_columns):
+    """Create dataframe for burst data for 1-spot ALEX"""
+    ncols = len(list_of_columns)
+    nrows = max(len(x) for x in list_of_columns)
+    columns = np.arange(ncols)
+    df = pd.DataFrame(columns=columns, index=np.arange(nrows), dtype=float)
+    df.columns.name = 'spot'
+    for col, col_data in zip(columns, list_of_columns):
+        df.iloc[:len(col_data), col] = col_data
+    return df
+
+
+def make_df_bursts(list_of_columns):
+    """Create 48-column dataframe for 48-spot PAX data"""
+    ncols = 48
+    assert len(list_of_columns) == ncols
+    nrows = max(len(x) for x in list_of_columns)
+    columns = np.arange(ncols)
+    df = pd.DataFrame(columns=columns, index=np.arange(nrows), dtype=float)
+    df.columns.name = 'spot'
+    for col, col_data in zip(columns, list_of_columns):
+        df.iloc[:len(col_data), col] = col_data
+    return df
+
+
+def make_df_spots(list_of_tuples=None):
+    """Create 48-rows dataframe for 48-spot PAX data"""
+    nrows = 48
+    df = pd.DataFrame(index=np.arange(nrows))
+    if list_of_tuples is None:
+        list_of_tuples = []
+    for col, col_data in list_of_tuples:
+        df[col] = col_data
+    return df
+
+
+def cal_phrate(d, stream, phrates=None, recompute=False):
+    Path('results').mkdir(exist_ok=True)
+    if phrates is None:
+        phrates = {}
+    phrates_fname = Path('results/%s_phrate_%s.csv' % (mlabel, stream))
+    phrates_fnameB = Path('results/%s_phrate_%sB.csv' % (mlabel, stream))
+    if phrates_fname.is_file() and not recompute:
+        phrates[str(stream)] = pd.read_csv(phrates_fname, index_col=0)
+        phrates[str(stream)].index.name = 'spot'
+        phr = pd.read_csv(phrates_fnameB, index_col=0)
+        phr.columns = [int(c) for c in phr.columns]
+        phr.columns.name = 'spot'
+        phrates[str(stream)+'B'] = phr
+    else:
+        try:
+            d.calc_max_rate(m=10, ph_sel=stream, compact=True)
+        except ValueError:
+            d.calc_max_rate(m=10, ph_sel=stream, compact=False)
+        phrates[str(stream)+'B'] = make_df_bursts(d.max_rate)
+        phrates[str(stream)] = (make_df_spots()
+                        .assign(**{'num_bursts': d.num_bursts})
+                        .assign(**{'num_nans': [np.isnan(x).sum() for x in d.max_rate]})
+                        .assign(**{'num_valid': lambda x: x.num_bursts - x.num_nans})
+                        .assign(**{'valid_fraction': lambda x: 100 * x.num_valid / x.num_bursts})
+                      )
+        phrates[str(stream)].to_csv(phrates_fname)
+        phrates[str(stream)+'B'].to_csv(phrates_fnameB)
+
+    print('   Valid fraction (mean of all ch): %.1f %%' %
+          phrates[str(stream)].valid_fraction.mean())
+    return phrates
+
+
+def info_html(d):
+    """Display measurement info in the notebook"""
+    Dex, Aex = d.setup['excitation_input_powers']*1e3
+    s = """
+    <h3>File: &nbsp; &nbsp; &nbsp; {fname}</h3>
+    <blockquote><p class="lead">{descr}</p></blockquote>
+    <ul>
+    <li><span style='display: inline-block; width: 150px;'>Acquisition duration:</span> {time:.1f} s </li>
+    <li><span style='display: inline-block; width: 150px;'>Laser power:</span>  <b>{Dex:.0f}mW</b> @ 532nm &nbsp;&nbsp;&nbsp;  
+                                                                                <b>{Aex:.0f}mW</b> @ 628nm </li>
+    <li><span style='display: inline-block; width: 150px;'>ALEX period [offset]: </span> {period} ({period_us:.1f} μs)  [{offset}] </li></ul>
+    """.format(fname=d.fname, time=float(d.acquisition_duration), Dex=Dex, Aex=Aex, 
+               period=d.alex_period, period_us=d.alex_period*d.clk_p*1e6, offset=d.offset,
+               descr=d.description.decode())
+    return HTML(s)
+
+
+def save_name(name, folder='.', label=None, nospaces=False):
+    """Compute file name for saving a figure"""
+    if label is None:
+        label = mlabel
+    sname = '%s/%s_%s' % (folder, label, name)
+    if nospaces:
+        sname = sname.replace(' ', '_')
+    return sname 
+    
+def savefig(name, nospaces=True, label=None, **kwargs):
+    """Save a figure prepending the measurement label and other options"""
+    if not save_figures:
+        return
+    savefigpath = Path(savefigdir)
+    savefigpath.mkdir(exist_ok=True)
+    kwargs_ = dict(dpi=100, bbox_inches='tight')
+                   #frameon=True, facecolor='white', transparent=False)
+    kwargs_.update(kwargs)
+    fname = save_name(name, savefigdir, nospaces=nospaces, label=label)
+    plt.savefig(fname, **kwargs_)
+    print('Saved: %s.png' % fname, flush=True)
+    if highres:
+        kwargs_['dpi'] = 300
+        fname = save_name(name.strip('.png') + '_highres', savefigdir, nospaces=nospaces, label=label)
+        print('Saved hires: %s.png' % fname, flush=True)
+        plt.savefig(fname, **kwargs_)
